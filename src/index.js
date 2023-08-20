480c6de6a11c15d5246c8add4236964ce21fc606
MashaZherebtsova <113789649+MashaZherebtsova@users.noreply.github.com>
2023-08-20 10:01:52 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/your-pet-project-group1">
       <App />
     </BrowserRouter>
-    
   </React.StrictMode>
 );
