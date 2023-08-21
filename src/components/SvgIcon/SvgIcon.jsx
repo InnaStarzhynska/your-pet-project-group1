4712e080a50a550f8cf9356bd490a7a3dcbb1089
InnaStarzhynska <113523884+InnaStarzhynska@users.noreply.github.com>
2023-08-22 00:31:00 +0300

    changed sprite and SvgIcon

--- src/components/SvgIcon/SvgIcon.jsx
+++ src/components/SvgIcon/SvgIcon.jsx
@@ -1,9 +1,10 @@
 import icons from '../../images/icons.svg';
+import { colors } from 'constants/colors';
 
-export default function SvgIcon({ id }) {
-  return (
-    <svg width={24} height={24}>
-      <use href={`${icons}#${id}`}></use>
+export default function SvgIcon({ id, color=colors.blueColor }) {
+    return (
+        <svg width={24} height={24} stroke={color}>
+          <use href={`${icons}#${id}`}></use>
     </svg>
   );
 }
