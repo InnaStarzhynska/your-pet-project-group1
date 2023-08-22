65dd17aa9776dae61a15475f774b3a27748401c0
IrynaYermolova <ermolova_ira@ukr.net>
2023-08-22 15:12:06 +0300

    Add LoginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,131 @@
+import { useState } from 'react';
+
+import { Link } from 'react-router-dom';
+import { Formik, Form, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import {
+  Button,
+  BtnContainer,
+  ErrorWrap,
+  FormContainer,
+  Input,
+  InputContainer,
+  InputWrap,
+  Title,
+} from './LoginForm.styled';
+import SvgIcon from 'components/SvgIcon/SvgIcon';
 
 
-export default function LoginForm() {
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valide email')
+    .required('Email is required'),
+  password: yup.string('Enter your password').required('Password is required'),
+});
+
+
+const initialValues = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+
+
+const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+  
+  const handleSubmit = (values, {resetForm}) => {
+    console.log(values);
+    resetForm();
+      }
+   
+
+  // const togglePassword = () => {
+  //   setShowPassword(!showPassword);
+  // };
+
+
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectIsLoading)
+
+ 
+  //   validationSchema: validationSchema,
+  //   onSubmit: (values, { resetForm }) => {
+  //     dispatch(login(values))
+  //     resetForm();
+  //   },
+  // });
+
   return (
-    <div>LoginForm</div>
-  )
-}
+
+   <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ isSubmitting, errors, touched }) => (
+        <Form autoComplete="off">
+          <FormContainer>
+            <InputContainer>
+              <Title>Login</Title>
+              <InputWrap>
+                <label htmlFor="email" hidden>
+                  Email
+                </label>
+
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                />
+              </InputWrap>
+              {touched.email && !errors.email ? (
+                <div>Email is secure</div>
+              ) : (
+                <ErrorWrap>
+                  <ErrorMessage name="email" component="div" />
+                </ErrorWrap>
+              )}
+
+              <InputWrap>
+                <label htmlFor="password" hidden>
+                  Password
+                </label>
+
+                <Input
+                  id="password"
+                  type={setShowPassword ? 'text' : 'password'}
+                  name="password"
+                  placeholder="Password"
+                >  {showPassword ? <SvgIcon iconName={'icon-eye-open'} /> : <SvgIcon iconName={'icon-eye-closed'} />}</Input>
+              </InputWrap>
+              {touched.password && !errors.password ? (
+                <div>Password is secure</div>
+              ) : (
+                <ErrorWrap>
+                  <ErrorMessage name="password" component="div" />
+                </ErrorWrap>
+              )}
+            </InputContainer>
+            <BtnContainer>
+              <Button type="submit" disabled={isSubmitting}>
+                Login
+              </Button>
+              <p>
+                Don't have an account? <Link to="/register">Register</Link>
+              </p>
+            </BtnContainer>
+          </FormContainer>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+
+export default LoginForm;
---
+++ src/components/LoginForm/LoginForm.styled.js
@@ -0,0 +1,149 @@
+import { Field } from 'formik';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+box-sizing: border-box;
+width: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-left: auto;
+margin-right: auto;
+padding: 40px 12px;
+gap: 16px;
+background-color: #ffffff;
+box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
+
+border-radius: 20px;
+
+@media screen and (min-width: 480px) {
+  max-width: 480px;
+  border-radius: 40px;
+}
+
+
+  @media screen and (min-width: 768px) {
+    max-width: 608px;
+    padding: 60px 75px;
+    border-radius: 40px;
+    gap: 52px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    gap: 28px;
+  }
+`;
+
+export const Title = styled.h2`
+box-sizing: border-box;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 1.38;
+  color: #111111;
+  margin-bottom: 40px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 36px;
+  letter: 4%;
+    font-weight: 500;
+  }
+
+`
+ export const InputContainer = styled.div`
+ box-sizing: border-box;
+  width: 100%;
+   display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 24px;
+ 
+   `
+export const InputWrap = styled.div`
+box-sizing: border-box;
+width: 100%;
+   position: relative;
+   display: flex;
+   flex-direction: column;
+  align-items: center;
+ 
+  
+`
+
+export const BtnContainer = styled.div`
+box-sizing: border-box;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+`
+
+
+export const Input = styled(Field)`
+box-sizing: border-box;
+  width: 100%;
+  outline: 1px solid #54adff;
+  border: none;
+  border-radius: 40px;
+  padding: 12px 42px 12px 16px;
+  font-size: 16px;
+  line-height: 1.5;
+  letter-spacing: 0.04em;
+  color: #888888;
+`
+export const ErrorWrap = styled.div`
+ box-sizing: border-box;
+ display: flex;
+  width: 100%;
+  padding-left: 12px;
+   align-items: center;
+   justify-content: left;
+  height: 24px;
+   font-size: 12px;
+   color: #f43f5e;
+   font-size: 14px;
+   font-weight: 400;
+
+
+   @media screen and (min-width: 768px) {
+    height: 32px;
+ `
+ export const Button = styled.button`
+ width: 100%;
+  gap: 8px;
+  padding: 11px 0;
+  margin-bottom: 24px;
+  color: #ffffff;
+  background-color: #54adff;
+  border-radius: 40px;
+  border: none;
+  outline: none;
+  font-family: inherit;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 1.35;
+  letter-spacing: 0.04em;
+  cursor: pointer;
+ 
+
+&:hover {
+  background: linear-gradient(290.46deg, #419ef1 0%, #9bd0ff 107.89%);
+}`
+
+export const Text = styled.p`
+  font-size: 12px;
+  line-height: 1.33;
+  letter-spacing: 0.04em;
+  text-align: center;
+  color: #888888;
+`
+
+export const StyledLink = styled(Link)`
+  font-size: 12px;
+  line-height: 1.33;
+  letter-spacing: 0.04em;
+  text-decoration: underline;
+  display: inline-block;
+  color: #54adff;
+`
