4dd1ae9c3e55a4ee93d354412e4f82a1e536ed41
InnaStarzhynska <113523884+InnaStarzhynska@users.noreply.github.com>
2023-08-22 23:36:11 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { persistor, store } from 'redux/store';
+import { Provider } from "react-redux";
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/your-pet-project-group1">
+      <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
+        </PersistGate>
+        </Provider>
     </BrowserRouter>
     
   </React.StrictMode>
---
+++ src/redux/store.js
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { authReducer } from './slices/AuthSlice';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
