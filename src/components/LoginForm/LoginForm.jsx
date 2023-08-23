bdc8ada087e8dde410917f5e4c43c89181a5dcab
IrynaYermolova <ermolova_ira@ukr.net>
2023-08-23 23:54:21 +0300

    update loginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,131 +1,205 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Formik } from "formik";
+import * as Yup from 'yup';
+import { logIn } from "../../redux/operations";
 
-import { Link } from 'react-router-dom';
-import { Formik, Form, ErrorMessage } from 'formik';
-import * as yup from 'yup';
 import {
-  Button,
-  BtnContainer,
-  ErrorWrap,
+  InputWrapper,
   FormContainer,
+  PassWrapper,
   Input,
-  InputContainer,
-  InputWrap,
-  Title,
-} from './LoginForm.styled';
-import SvgIcon from 'components/SvgIcon/SvgIcon';
+  ShowPassBtn,
+  IconShown,
+  IconHidden,
+  Btn,
+  LoginHeader,
+  Text,
+  WrapperCheckCross,
+  IconCheck,
+  IconCross,
+  WrapperInput,
+  WrapperShowCheck,
+  Correct,
+  Error,
+} from "./LoginForm.styled";
 
+const emailValid =
+  /^([a-zA-Z0-9]{1}[a-zA-Z0-9_\-.]{1,})@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,4})$/;
+const passwordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]/;
 
-
-const validationSchema = yup.object({
-  email: yup
-    .string('Enter your email')
-    .email('Enter a valide email')
-    .required('Email is required'),
-  password: yup.string('Enter your password').required('Password is required'),
+const LoginSchema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email field is required")
+    .matches(emailValid, "Enter a valid Email")
+    .max(70, "Maximum 70 characters")
+    .min(10, "Minimum 10 characters")
+    .email(),
+  password: Yup.string()
+    .matches(passwordValid, "Please enter min 6 characters and max 16")
+    .max(16, "Please enter 16 characters or less")
+    .min(6, "Enter 6 or more characters")
+    .required("Password field is required"),
 });
 
-
-const initialValues = {
-  email: '',
-  password: '',
-  confirmPassword: '',
+const InputError = ({ name }) => {
+  return (
+    <Error>
+      <p style={{ margin: 0 }}>{name}</p>
+    </Error>
+  );
 };
 
-
+const InputCorrect = ({ name }) => {
+  return (
+    <Correct>
+      <p style={{ margin: 0 }}>{name}</p>
+    </Correct>
+  );
+};
 
 const LoginForm = () => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [isPasswordShown, setIsPasswordShown] = useState(false);
-  
-  const handleSubmit = (values, {resetForm}) => {
-    console.log(values);
+  const dispatch = useDispatch();
+
+  const [password, setPassword] = useState(false);
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    const { email, password } = values;
+    dispatch(
+      logIn({
+        email: email,
+        password: password,
+      })
+    );
+
     resetForm();
-      }
-   
+  };
 
-  // const togglePassword = () => {
-  //   setShowPassword(!showPassword);
-  // };
-
-
-  // const dispatch = useDispatch();
-  // const isLoading = useSelector(selectIsLoading)
-
- 
-  //   validationSchema: validationSchema,
-  //   onSubmit: (values, { resetForm }) => {
-  //     dispatch(login(values))
-  //     resetForm();
-  //   },
-  // });
+  const togglePassword = () => setPassword(!password);
 
   return (
-
-   <Formik
+    <Formik
       initialValues={initialValues}
       onSubmit={handleSubmit}
-      validationSchema={validationSchema}
+      validationSchema={LoginSchema}
     >
-      {({ isSubmitting, errors, touched }) => (
-        <Form autoComplete="off">
-          <FormContainer>
-            <InputContainer>
-              <Title>Login</Title>
-              <InputWrap>
-                <label htmlFor="email" hidden>
-                  Email
-                </label>
-
+      {({ errors, touched, values }) => (
+        <FormContainer>
+          <LoginHeader>Login</LoginHeader>
+          <InputWrapper>
+            <PassWrapper>
+              <WrapperInput
+                className={
+                  (values.email === "" && "default") ||
+                  (touched.email && errors.email && "error") ||
+                  (!errors.email && "success")
+                }
+              >
                 <Input
-                  id="email"
                   type="email"
                   name="email"
-                  placeholder="Email"
+                  id="email"
+                  autoComplete="off"
+                  placeholder={"Email"}
                 />
-              </InputWrap>
-              {touched.email && !errors.email ? (
-                <div>Email is secure</div>
-              ) : (
-                <ErrorWrap>
-                  <ErrorMessage name="email" component="div" />
-                </ErrorWrap>
-              )}
-
-              <InputWrap>
-                <label htmlFor="password" hidden>
-                  Password
-                </label>
-
+                <WrapperCheckCross>
+                  {(values.email === "" && true) ||
+                    (touched.email && errors.email && <IconCross />) ||
+                    (!errors.email && <IconCheck />)}
+                </WrapperCheckCross>
+              </WrapperInput>
+              {values.email === "" && true
+                ? null
+                : errors.email &&
+                  touched.email !== "" && (
+                    <InputError name="Enter a valid Email" />
+                  )}
+            </PassWrapper>
+            <PassWrapper>
+              <WrapperInput
+                className={
+                  (values.password === "" && "default") ||
+                  (touched.password && errors.password && "error") ||
+                  (!errors.password && "success")
+                }
+              >
                 <Input
-                  id="password"
-                  type={setShowPassword ? 'text' : 'password'}
+                  type={password ? "text" : "password"}
                   name="password"
-                  placeholder="Password"
-                >  {showPassword ? <SvgIcon iconName={'icon-eye-open'} /> : <SvgIcon iconName={'icon-eye-closed'} />}</Input>
-              </InputWrap>
-              {touched.password && !errors.password ? (
-                <div>Password is secure</div>
-              ) : (
-                <ErrorWrap>
-                  <ErrorMessage name="password" component="div" />
-                </ErrorWrap>
-              )}
-            </InputContainer>
-            <BtnContainer>
-              <Button type="submit" disabled={isSubmitting}>
-                Login
-              </Button>
-              <p>
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </BtnContainer>
-          </FormContainer>
-        </Form>
+                  id="password"
+                  autoComplete="off"
+                  placeholder={"Password"}
+                />
+                <WrapperShowCheck>
+                  <ShowPassBtn
+                    type="button"
+                    onClick={togglePassword}
+                    data-shown={password}
+                    className="btnShowHidden"
+                  >
+                    {password ? (
+                      <IconShown viewBox="0 0 32 32">
+                        <path
+                          fill="none"
+                          stroke="#54adff"
+                          strokeLinejoin="round"
+                          strokeLinecap="round"
+                          strokeMiterlimit="4"
+                          strokeWidth="2"
+                          d="M2.667 16c0 0 4.848-9.333 13.333-9.333s13.333 9.333 13.333 9.333-4.849 9.333-13.333 9.333c-8.485 0-13.333-9.333-13.333-9.333z"
+                        ></path>
+                        <path
+                          fill="none"
+                          stroke="#54adff"
+                          strokeLinejoin="round"
+                          strokeLinecap="round"
+                          strokeMiterlimit="4"
+                          strokeWidth="2"
+                          d="M16 20c2.209 0 4-1.791 4-4s-1.791-4-4-4c-2.209 0-4 1.791-4 4s1.791 4 4 4z"
+                        ></path>
+                      </IconShown>
+                    ) : (
+                      <IconHidden viewBox="0 0 32 32">
+                        <path
+                          fill="none"
+                          stroke="#54adff"
+                          strokeLinejoin="round"
+                          strokeLinecap="round"
+                          strokeMiterlimit="4"
+                          strokeWidth="2"
+                          d="M13.019 7.060c0.936-0.251 1.931-0.394 2.981-0.394 8.485 0 13.333 9.333 13.333 9.333s-1.105 2.128-3.184 4.351M5.799 11.704c-2.045 2.203-3.132 4.296-3.132 4.296s4.848 9.333 13.333 9.333c1.069 0 2.081-0.148 3.033-0.407M15.333 19.945c-1.446-0.243-2.628-1.26-3.106-2.611M16.667 12.055c1.675 0.281 2.997 1.603 3.278 3.278M4 4l24 24"
+                        ></path>
+                      </IconHidden>
+                    )}
+                  </ShowPassBtn>
+                  {(values.password === "" && true) ||
+                    (touched.password && errors.password && <IconCross />)}
+
+                  {(values.password === "" && true) ||
+                    (!errors.password && <IconCheck />)}
+                </WrapperShowCheck>
+              </WrapperInput>
+              {values.password === "" && true
+                ? null
+                : !errors.password &&
+                  touched.password !== "" && (
+                    <InputCorrect name="Password is secure" />
+                  )}
+            </PassWrapper>
+          </InputWrapper>
+          <Btn type="submit">Login</Btn>
+          <Text>
+            Don't have an account?<a href="/register">Register</a>
+          </Text>
+        </FormContainer>
       )}
     </Formik>
   );
 };
-
 
 export default LoginForm;
--- src/components/LoginForm/LoginForm.styled.js
+++ src/components/LoginForm/LoginForm.styled.js
@@ -1,149 +1,212 @@
-import { Field } from 'formik';
-import {Link} from 'react-router-dom';
-import styled from 'styled-components';
+import styled from "styled-components";
+import { Form, Field } from "formik";
 
-export const FormContainer = styled.div`
-box-sizing: border-box;
-width: 100%;
-display: flex;
-flex-direction: column;
-align-items: center;
-margin-left: auto;
-margin-right: auto;
-padding: 40px 12px;
-gap: 16px;
-background-color: #ffffff;
-box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
+import { ReactComponent as CloseSvg } from "../../images/eye-closed.svg";
+import { ReactComponent as CheckSvg } from "../../images/check.svg";
 
-border-radius: 20px;
-
-@media screen and (min-width: 480px) {
-  max-width: 480px;
-  border-radius: 40px;
-}
-
-
-  @media screen and (min-width: 768px) {
-    max-width: 608px;
-    padding: 60px 75px;
-    border-radius: 40px;
-    gap: 52px;
-  }
-
-  @media screen and (min-width: 1280px) {
-    gap: 28px;
-  }
-`;
-
-export const Title = styled.h2`
-box-sizing: border-box;
-  font-weight: 500;
-  font-size: 24px;
-  line-height: 1.38;
-  color: #111111;
-  margin-bottom: 40px;
-
-  @media screen and (min-width: 768px) {
-    font-size: 36px;
-  letter: 4%;
-    font-weight: 500;
-  }
-
-`
- export const InputContainer = styled.div`
- box-sizing: border-box;
-  width: 100%;
-   display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 24px;
- 
-   `
-export const InputWrap = styled.div`
-box-sizing: border-box;
-width: 100%;
-   position: relative;
-   display: flex;
-   flex-direction: column;
-  align-items: center;
- 
-  
-`
-
-export const BtnContainer = styled.div`
-box-sizing: border-box;
-  width: 100%;
+export const FormContainer = styled(Form)`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  margin-top: 80px;
 
-`
+  position: relative;
+  width: 280px;
+  height: 425px;
+  background-color: #fff;
+  box-shadow: 7px 13px 14px rgba(116, 177, 232, 0.24);
+  border-radius: 25px;
 
+  @media (min-width: 768px) {
+    width: 600px;
+    height: 480px;
+    border-radius: 50px;
+  }
+`;
+
+export const Error = styled.div`
+  margin-top: 4px;
+  margin-left: 20px;
+  width: 100%;
+  font-size: 12px;
+  font-family: Manrope, sans-serif;
+  color: red;
+`;
+
+export const Correct = styled(Error)`
+  color: green;
+`;
+
+export const LoginHeader = styled.h1`
+  font-size: 24px;
+  font-family: Manrope, sans-serif;
+  font-weight: 500;
+  margin-bottom: 20px;
+  margin-top: 40px;
+
+  @media (min-width: 768px) {
+    font-size: 36px;
+    margin-bottom: 40px;
+    margin-top: 0;
+  }
+`;
+
+export const InputWrapper = styled.div`
+  max-width: 100%;
+  width: 256px;
+  height: 50px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+
+  @media (min-width: 768px) {
+    width: 460px;
+    padding-bottom: 15px;
+    margin-bottom: 60px;
+  }
+`;
 
 export const Input = styled(Field)`
-box-sizing: border-box;
   width: 100%;
-  outline: 1px solid #54adff;
-  border: none;
-  border-radius: 40px;
-  padding: 12px 42px 12px 16px;
+
   font-size: 16px;
-  line-height: 1.5;
-  letter-spacing: 0.04em;
   color: #888888;
-`
-export const ErrorWrap = styled.div`
- box-sizing: border-box;
- display: flex;
-  width: 100%;
-  padding-left: 12px;
-   align-items: center;
-   justify-content: left;
-  height: 24px;
-   font-size: 12px;
-   color: #f43f5e;
-   font-size: 14px;
-   font-weight: 400;
 
+  background-color: #fff;
 
-   @media screen and (min-width: 768px) {
-    height: 32px;
- `
- export const Button = styled.button`
- width: 100%;
-  gap: 8px;
-  padding: 11px 0;
-  margin-bottom: 24px;
-  color: #ffffff;
-  background-color: #54adff;
-  border-radius: 40px;
-  border: none;
   outline: none;
-  font-family: inherit;
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 1.35;
-  letter-spacing: 0.04em;
-  cursor: pointer;
- 
+  border: none;
+`;
 
-&:hover {
-  background: linear-gradient(290.46deg, #419ef1 0%, #9bd0ff 107.89%);
-}`
+export const WrapperInput = styled.div`
+  display: flex;
+  padding-left: 12px;
+  padding-right: 12px;
+  align-items: center;
+  background-color: #fff;
+  border: 1px solid #54adff;
+  border-radius: 25px;
+  height: 45px;
+
+  &.default {
+    border-color: #54adff;
+  }
+  &.success {
+    border-color: #00c3ad;
+  }
+  &.error {
+    border-color: #f43f5e;
+  }
+
+  @media (min-width: 768px) {
+    border-radius: 25px;
+  }
+`;
+
+export const Btn = styled.button`
+  width: 256px;
+  height: 50px;
+  padding: 16px 32px;
+  z-index: 10;
+  margin-top: 170px;
+  margin-bottom: 20px;
+
+  font-weight: 500;
+  font-size: 20px;
+  font-family: Manrope, sans-serif;
+  color: #fff;
+  background-color: #54adff;
+
+  border: 1px solid rgba(255, 255, 255, 0.7);
+  border-radius: 25px;
+
+  transition: all 250ms ease-in-out;
+
+  cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: 460px;
+    border-radius: 25px;
+    font-size: 16px;
+    margin-top: 100px;
+  }
+`;
 
 export const Text = styled.p`
-  font-size: 12px;
-  line-height: 1.33;
-  letter-spacing: 0.04em;
-  text-align: center;
-  color: #888888;
-`
+  margin-bottom: 20px;
 
-export const StyledLink = styled(Link)`
-  font-size: 12px;
-  line-height: 1.33;
-  letter-spacing: 0.04em;
-  text-decoration: underline;
-  display: inline-block;
-  color: #54adff;
-`
+  font-family: Manrope, sans-serif;
+  font-size: 14px;
+  color: #888888;
+  text-shadow: 0px 1px 1px rgba(0, 0, 0, 0.2);
+
+  a {
+    color: #54adff;
+    text-decoration: underline;
+  }
+`;
+
+export const PassWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  &:first-child {
+    margin-bottom: 32px;
+  }
+`;
+
+export const ShowPassBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 5px;
+  width: 33px;
+  height: 33px;
+
+  background-color: transparent;
+  outline: none;
+  border: none;
+
+  cursor: pointer;
+`;
+
+export const IconHidden = styled.svg`
+  width: 24px;
+  height: 24px;
+`;
+
+export const IconShown = styled.svg`
+  width: 24px;
+  height: 24px;
+`;
+
+export const WrapperCheckCross = styled.div`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const IconCross = styled(CloseSvg)`
+  stroke: #f43f5e;
+`;
+
+export const IconCheck = styled(CheckSvg)`
+  width: 24px;
+  height: 24px;
+  * {
+    stroke: #00c3ad;
+  }
+`;
+
+export const WrapperShowCheck = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
