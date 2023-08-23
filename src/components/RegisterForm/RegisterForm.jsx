09fb5af6fec037ee71e1320f504daff8f13cabdd
InnaStarzhynska <113523884+InnaStarzhynska@users.noreply.github.com>
2023-08-23 11:20:28 +0300

    fix RegisterForm

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,5 @@
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 import {
