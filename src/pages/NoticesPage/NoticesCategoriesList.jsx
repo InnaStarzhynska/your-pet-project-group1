400bb830b093d03e9cebc28828accc9face3d43a
InnaStarzhynska <113523884+InnaStarzhynska@users.noreply.github.com>
2023-08-29 11:57:30 +0300

    fix

--- src/pages/NoticesPage/NoticesCategoriesList.jsx
+++ src/pages/NoticesPage/NoticesCategoriesList.jsx
@@ -1,19 +1,21 @@
 import { useSelector } from 'react-redux';
 import { NoticesList } from './NoticesPage.styled';
-import { selectNotices } from 'redux/selectors';
+import { selectNoticeById, selectNotices } from 'redux/selectors';
 import NoticesCategoryItem from './NoticeCategoryItem';
 import { useState } from 'react';
 import ModalNotice from './ModalNotice';
 
 export default function NoticesCategoriesList() {
-  const [isModalOpen, setModalOpen] = useState(false);
   const notices = useSelector(selectNotices);
+  const item = useSelector(selectNoticeById);
+  const [isModalOpen, setModalOpen] = useState(Object.keys(item).length);
+  
    const toggleModal = () => {
     setModalOpen(prevState => !prevState);
   };
   return (
     <><NoticesList>
-      {isModalOpen ? (<ModalNotice isOpenModal={toggleModal}/>) : ( notices.map(item => {
+      {isModalOpen ? (<ModalNotice isModalOpen={toggleModal}/>) : ( notices.map(item => {
         return <NoticesCategoryItem isModalOpen={setModalOpen} item={item} key={item._id} />
     }))}
       </NoticesList >
