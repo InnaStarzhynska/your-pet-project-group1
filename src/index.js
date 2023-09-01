da1f436d27d9effc7b7ce71e2d43bc1dd4da7933
Streltsov Valerii <st.valera04@gmail.com>
2023-09-01 17:21:45 +0300

    fix strict mode

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { persistor, store } from 'redux/store';
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/your-pet-project-group1">
-      <Provider store={store}>
+  // <React.StrictMode>
+  <BrowserRouter basename="/your-pet-project-group1">
+    <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <App />
-        </PersistGate>
-        </Provider>
-    </BrowserRouter>
-    
-  </React.StrictMode>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>
+
+  /* </React.StrictMode> */
 );
